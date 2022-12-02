6ba6471aa0b7e6a95356e358f239e3c36ab278de
rondi91 <rondie91@gmail.com>
2022-12-02 19:39:59 +0700

    add cart ,place order & order item

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -27,11 +27,140 @@
         <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         <!-- header start -->
-        @include('layouts.header')
+        {{-- @include('layouts.header')
         <!-- header end -->    
         @include('layouts.slider')
-       
-		
+        --}}
+		<div class="header-top-wrapper-2 border-bottom-2">
+            <div class="header-info-wrapper pl-200 pr-200">
+                <div class="header-contact-info">
+                    <ul>
+                        <li><i class="pe-7s-call"></i> +011 2231 4545</li>
+                        <li><i class="pe-7s-mail"></i> <a href="#">company@domail.info</a></li>
+                    </ul>
+                </div>
+                <div class="electronics-login-register">
+                    <ul>
+                        <li><a href="{{route('home')}}"><i class="pe-7s-users"></i>Home</a></li>
+                        <li><a href="#"><i class="pe-7s-users"></i>My Account</a></li>
+                        <li><a data-toggle="modal" data-target="#exampleCompare" href="#"><i
+                                    class="pe-7s-repeat"></i>Compare</a></li>
+                        {{-- <li><a href="wishlist.html"><i class="pe-7s-like"></i>Wishlist</a></li> --}}
+
+                        @auth
+                        
+                        <li>
+                                <form action=" {{ url('/logout') }} " method="POST">
+                                    @csrf
+                                    <input type="submit" value="Logout" />
+                                </form>
+                        </li>
+                            @else
+
+                            <li>
+                                <a href="{{url('/login')}}">Login</a>
+                            </li>
+                            <li>
+                                <a href="{{url('/register')}}">Sign Up</a>
+                            </li>
+                        @endauth
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="header-bottom pt-40 pb-30 clearfix">
+            <div class="header-bottom-wrapper pr-200 pl-200">
+                <div class="logo-3">
+                    <a href="{{route('home')}}">
+                        <img src="/assets/img/logo/logo-3.png" alt="">
+                    </a>
+                </div>
+                <div class="categories-search-wrapper">
+
+                    <div class="categories-wrapper">
+                        {{-- <form action="{{route('products.search')}}" method="GET"> --}}
+                        <form action="" method="GET">
+                            <input name="query" placeholder="Enter Your key word" type="text">
+                            <button type="submit"> Search </button>
+                        </form>
+                    </div>
+                </div>
+                <div class="trace-cart-wrapper">
+                    <div class="trace same-style">
+                        <div class="same-style-icon">
+                            <a href="#"><i class="pe-7s-plane"></i></a>
+                        </div>
+                        <div class="same-style-text">
+                            <a href="#">Product <br>trace</a>
+                        </div>
+                    </div>
+                    <div class="categories-cart same-style">
+                        <div class="same-style-icon">
+                            <a href="{{ route('cart.list') }}"><i class="pe-7s-cart"></i></a>
+                        </div>
+                        <div class="same-style-text">
+                            <a href="{{ route('cart.list') }}">My Cart <br>
+
+                                @auth
+                                {{Cart::session(auth()->id())->getContent()->count()}}
+                                @else
+                                0
+                                @endauth
+
+                                Item</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mobile-menu-area electro-menu d-md-block col-md-12 col-lg-12 col-12 d-lg-none d-xl-none">
+                    <div class="mobile-menu">
+                        <nav id="mobile-menu-active">
+                            <ul class="menu-overflow">
+                                <li><a href="{{url('/')}}">HOME</a>
+
+                                </li>
+                                <li><a href="#">BLOG</a>
+
+                                </li>
+                                <li><a href="#"> Contact </a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header end -->
+
+
+    @if(session('message'))
+
+        <div class="alert alert-success text-center" role="alert">
+            <strong>{{ session('message') }}</strong>
+        </div>
+    @endif
+
+    @if(session('error'))
+
+        <div class="alert alert-danger text-center" role="alert">
+            <strong>{{session('error')}} </strong>
+        </div>
+    @endif
+
+        {{-- display success message --}}
+        @if(session()->has('message'))
+            <div class="alert alert-success text-center" role="alert">
+               {{session('message')}}
+            </div>
+        @endif
+
+        {{-- display error message --}}
+
+        @if(session()->has('error'))
+        <div class="alert alert-danger text-center" role="alert">
+            {{session('error')}}
+        </div>
+        @endif
 		
 		
 		@yield('content')
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
@@ -20,7 +21,7 @@ Route::get('/', function () {
     return view('shops.index',[
         'products'=>Product::take(8)->get()
     ]);
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -30,17 +31,28 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+
+    // ORDER 
+
+// Route::post('order', [OrderController::class, 'store'])->name('orders.store');
+Route::resource('orders', OrderController::class);
+
 
 
 // cart 
-Route::get('/addCart/{product}', [CartController::class, 'addCart'])->name('cart.add')->middleware('auth');
-Route::get('cart', [CartController::class, 'cartlist'])->name('cart.list')->middleware('auth');
+Route::get('/addCart/{product}', [CartController::class, 'addCart'])->name('cart.add');
+Route::get('cart', [CartController::class, 'cartlist'])->name('cart.list');
 Route::get('cart/update/{itemId}', [CartController::class, 'updateCart'])->name('cart.update');
 Route::post('cart/destroy', [CartController::class, 'destroy'])->name('cart.remove');
 // Route::get('cart/destroy/id', [CartController::class, 'destroy'])->name('cart.remove');
 Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
 Route::get('/cart/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
+
+});
+
+
+
+
 
 
 
